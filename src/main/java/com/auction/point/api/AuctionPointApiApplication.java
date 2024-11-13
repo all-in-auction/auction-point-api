73f7a18c6272097574533ee2ab005eee62d75ec0
areum0116 <areum0116@naver.com>
2024-11-14 02:01:09 +0900

    chore : Add eureka

--- src/main/java/com/auction/point/api/AuctionPointApiApplication.java
+++ src/main/java/com/auction/point/api/AuctionPointApiApplication.java
@@ -2,9 +2,11 @@ package com.auction.point.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuctionPointApiApplication {
 
 	public static void main(String[] args) {
