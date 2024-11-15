356b64d79d7210734440f21c2805bf066825da8e
areum0116 <areum0116@naver.com>
2024-11-15 20:11:10 +0900

    feat(point-bid-gRPC) : Create methods in PointGrpcService

--- src/main/java/com/auction/point/grpc/PointGrpcService.java
+++ src/main/java/com/auction/point/grpc/PointGrpcService.java
@@ -1,6 +1,11 @@
 package com.auction.point.grpc;
 
+import com.auction.point.api.common.apipayload.status.ErrorStatus;
+import com.auction.point.api.common.exception.ApiException;
+import com.auction.point.api.domain.point.repository.PointRepository;
 import com.auction.point.api.domain.point.service.PointService;
+import com.auction.point.api.domain.pointHistory.enums.PaymentType;
+import com.auction.point.api.domain.pointHistory.service.PointHistoryService;
 import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,22 +20,80 @@ import point.PointServiceGrpc;
 public class PointGrpcService extends PointServiceGrpc.PointServiceImplBase {
 
     private final PointService pointService;
+    private final PointRepository pointRepository;
+    private final PointHistoryService pointHistoryService;
 
     @Override
     @Transactional
     public void createPoint(
             Point.CreatePointRequest request,
-            StreamObserver<Point.CreatePointResponse> responseObserver)
-    {
+            StreamObserver<Point.CreatePointResponse> responseObserver) {
 
+        Point.CreatePointResponse.Builder responseBuilder = Point.CreatePointResponse.newBuilder();
+        try {
             long userId = request.getUserId();
             pointService.createPoint(userId);
-            log.info("SUCCESS");
-            Point.CreatePointResponse response = Point.CreatePointResponse.newBuilder()
-                    .setStatus("SUCCESS")
-                    .build();
-
-            responseObserver.onNext(response);
+            responseBuilder.setStatus("SUCCESS");
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            responseBuilder.setStatus("ERROR");
+        } finally {
+            responseObserver.onNext(responseBuilder.build());
             responseObserver.onCompleted();
+        }
+    }
+
+    @Override
+    public void getPoints(Point.GetPointsRequest request, StreamObserver<Point.GetPointsResponse> responseObserver) {
+        Point.GetPointsResponse.Builder responseBuilder = Point.GetPointsResponse.newBuilder();
+        try {
+            long userId = request.getUserId();
+            com.auction.point.api.domain.point.entity.Point point = pointRepository.findByUserId(userId)
+                    .orElseThrow(() -> new ApiException(ErrorStatus._INVALID_REQUEST));
+
+            responseBuilder.setTotalPoint(point.getPointAmount());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new ApiException(ErrorStatus._INVALID_REQUEST);
+        } finally {
+            responseObserver.onNext(responseBuilder.build());
+            responseObserver.onCompleted();
+        }
+
+    }
+
+    @Override
+    public void decreasePoints(Point.DecreasePointsRequest request, StreamObserver<Point.DecreasePointsResponse> responseObserver) {
+        Point.DecreasePointsResponse.Builder responseBuilder = Point.DecreasePointsResponse.newBuilder();
+        try {
+            long userId = request.getUserId();
+            int price = request.getAmount();
+            pointService.decreasePoint(userId, price);
+            responseBuilder.setStatus("SUCCESS");
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            responseBuilder.setStatus("FAIL");
+        } finally {
+            responseObserver.onNext(responseBuilder.build());
+            responseObserver.onCompleted();
+        }
+    }
+
+    @Override
+    public void createPointHistory(Point.CreatePointHistoryRequest request, StreamObserver<Point.CreatePointHistoryResponse> responseObserver) {
+        Point.CreatePointHistoryResponse.Builder responseBuilder = Point.CreatePointHistoryResponse.newBuilder();
+        try {
+            long userId = request.getUserId();
+            int price = request.getAmount();
+            String paymentType = request.getPaymentType();
+            pointHistoryService.createPointHistory(userId, price, PaymentType.valueOf(paymentType));
+            responseBuilder.setStatus("SUCCESS");
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            responseBuilder.setStatus("FAIL");
+        } finally {
+            responseObserver.onNext(responseBuilder.build());
+            responseObserver.onCompleted();
+        }
     }
 }
