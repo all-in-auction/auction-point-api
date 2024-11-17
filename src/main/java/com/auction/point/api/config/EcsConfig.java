6c6ad7d74a15a065b1be90dfe8dd3a5e746cef13
inseooo0 <calmandpassion@korea.ac.kr>
2024-11-17 20:33:51 +0900

    hotfix(config): fix eureka config

---
+++ src/main/java/com/auction/point/api/config/EcsConfig.java
@@ -0,0 +1,35 @@
+package com.auction.point.api.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Configuration
+@Slf4j
+@Profile("prod")
+public class EcsConfig {
+
+    @Bean
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils){
+
+        EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+        String ip = null;
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+            log.info("ECS Task Container Private Ip address is {}", ip);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        config.setIpAddress(ip);
+        config.setPreferIpAddress(true);
+
+        return config;
+    }
+}
