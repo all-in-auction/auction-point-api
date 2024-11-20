e46448d9ea621d4feb0caa45538969f776ab0a3b
inseooo0 <calmandpassion@korea.ac.kr>
2024-11-20 22:42:30 +0900

    feat(config): add cors config for swagger

--- src/main/java/com/auction/point/api/config/WebConfig.java
+++ src/main/java/com/auction/point/api/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.auction.point.api.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +16,15 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/templates/", "classpath:/static/")
                 .setCacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/v3/api-docs")
+                .allowedOrigins("http://www.all-in-auction.site")
+                .allowedMethods("GET");
+        registry.addMapping("/swagger-ui/**")
+                .allowedOrigins("http://www.all-in-auction.site")
+                .allowedMethods("GET");
     }
 }
