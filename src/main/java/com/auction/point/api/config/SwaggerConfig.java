d28f62fe8f593aacb2e3e0133de3aaf9558b258b
inseooo0 <calmandpassion@korea.ac.kr>
2024-11-22 17:37:45 +0900

    fix(swagger): local test를 위한 swagger config 수정

--- src/main/java/com/auction/point/api/config/SwaggerConfig.java
+++ src/main/java/com/auction/point/api/config/SwaggerConfig.java
@@ -6,13 +6,18 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+
 
 @Configuration
-@Profile("prod")
+@RequiredArgsConstructor
 public class SwaggerConfig {
+
+    private final Environment environment;
+
     @Bean
     public OpenAPI openAPI() {
         String jwt = "JWT";
@@ -24,8 +29,15 @@ public class SwaggerConfig {
                 .bearerFormat("JWT")
         );
 
+        String activeProfile = environment.getProperty("spring.profiles.active", "dev");
+        String gatewayURL = "";
+        if ("prod".equals(activeProfile)) {
+            gatewayURL = "http://www.all-in-auction.site";
+        } else {
+            gatewayURL = "http://localhost:8080";
+        }
         Server gatewayServer = new Server();
-        gatewayServer.setUrl("http://www.all-in-auction.site");
+        gatewayServer.setUrl(gatewayURL);
         gatewayServer.setDescription("Gateway Server");
 
         return new OpenAPI()
